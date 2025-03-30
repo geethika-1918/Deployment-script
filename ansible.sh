5dbcf8fd682e072aee2832d06e24f89989df2c67
geethika-1918 <100533888+geethika-1918@users.noreply.github.com>
2025-03-30 14:57:15 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,10 +1,7 @@
-apt install software-properties-common
- 
-add-apt-repository --yes --update ppa:ansible/ansible
- 
-apt install ansible -y 
- 
-apt install docker.io -y
- 
-ansible --version
+apt update -y
+apt-get install -y software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
+
  
